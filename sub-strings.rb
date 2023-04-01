fb71148c170d1ee342b03694bc71201ac0746a38
Kike Mendoza <kike.mendoza9978@gmail.com>
2023-04-01 16:55:25 -0600

    Iterate a string by substrings

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,3 +1,5 @@
+require "pry-byebug"
+
 def substrings(word,arr) 
   word_arr = word.split
   result = arr.reduce(Hash.new(0)) do |result, item|
@@ -9,4 +11,16 @@ end
 
 
 dictionary = ["below","down","go","below","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below down",dictionary)
+# substrings("below down",dictionary)
+def iterate_substring(word)
+  string_range = (0..word.size)
+  i = 0
+  word.each_char do |c| 
+    puts word[(i..word.size)]
+    
+    i += 1
+  end
+end
+
+
+iterate_substring("hola")
