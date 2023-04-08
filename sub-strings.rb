0ad674119d1145c5842a5e3b3e787e2dafe57534
Kike Mendoza <kike.mendoza9978@gmail.com>
2023-04-08 12:08:40 -0600

    Swap code for using #scan and making it cleaner

--- sub-strings.rb
+++ sub-strings.rb
@@ -1,35 +1,17 @@
 require "pry-byebug"
 
-def substrings(word,arr) 
-  word_arr = word.downcase.split
-  result = arr.reduce(Hash.new(0)) do |result, item|
-    word_arr.each do |word| 
-      iterate_substring(word).each do |substring|
-        result[item] += 1 if substring == item 
-        # binding.pry
-        end
-      end
-    result
+def substrings(text, dictionary) 
+  result = Hash.new(0)
+  downcase_text = text.downcase
+
+  dictionary.each do |word|
+    # scan search in all the phrase for the string or regex in te argument, it return an array of th elements found
+    matches = downcase_text.scan(word).length
+    result[word] = matches unless matches == 0
   end
   result
 end
 
-
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def iterate_substring(word)
-  string_range = (0..word.size)
-  i = 0
-  
-  sub_string_arr = []
-  word.each_char do |c| 
-    substring = word[(i..word.size)]
-    sub_string_arr.push(substring)
-    i += 1
-  end
-  sub_string_arr
-end
-
-p substrings("below Howdy partner, sit down! How's it going?",dictionary)
-
-# p iterate_substring("hola")
+print substrings("Howdy partner, sit down! How's it going?", dictionary)
